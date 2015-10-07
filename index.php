36e7ae361be1ed0ba5da9059b25fa95af4d66dcb
Jean Gonçalves <jean@agenciasys.com.br>
2015-10-07 13:53:23 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Debugger | Chrome</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+    <table>
+        <tr>
+            <td>
+                <img src="https://upload.wikimedia.org/wikipedia/en/d/d9/Script_Debugger.png">
+            </td>
+            <td>
+                <div class="header">Desvendando o Debugger e o Chrome Dev Tools</div>
+                <div class="name">
+                    <blockquote>com Jean Gonçalves</blockquote>
+                </div>
+            </td>
+        </tr>
+    </table>
+
+    <div class="container">
+        <fieldset>
+            <pre>console.assert(expression, object);</pre>
+            <p>Quando a expressão é <b>FALSE</b>, ele exibe o <b>OBJECT</b> no console com forma de Erro.</p>
+            <a href="console/assert.html">Exemplo</a>
+        </fieldset>
+    </div>
+
+    <div class="container">
+        <fieldset>
+            <pre>console.count(label);</pre>
+            <p>Quando a expressão é <b>FALSE</b>, ele exibe o <b>OBJECT</b> no console com forma de Erro.</p>
+            <a href="console/assert.html">Exemplo</a>
+        </fieldset>
+    </div>
+
+</body>
+</html>
